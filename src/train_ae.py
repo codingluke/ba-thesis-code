d2bc3170cacf97a4dc7912f1088497ebf24381c4
Lukas Hodel <l.hodel@de.edenspiekermann.com>
2016-01-22 18:03:07 +0100

    Fixes validierung beim training

--- src/train_ae.py
+++ src/train_ae.py
@@ -18,19 +18,19 @@ border = 2
 BA1 = BatchImgProcessor.load(
     X_dirpath='../../data/train_cleaned/*',
     y_dirpath='../../data/train_cleaned/',
-    batchsize=50000,
+    batchsize=5000,
     border=border,
-    limit=20,
+    limit=10,
     train_stepover=8,
     dtype=theano.config.floatX)
-pretrain_data = BA1(modus='full', random=True)
+pretrain_data = BA1(modus='train', random=True)
 
 BatchProcessor = BatchImgProcessor.load(
     X_dirpath='../../data/train/*',
     y_dirpath='../../data/train_cleaned/',
-    batchsize=50000,
+    batchsize=5000,
     border=border,
-    limit=20,
+    limit=10,
     train_stepover=8,
     dtype=theano.config.floatX)
 training_data = BatchProcessor(modus='train', random=True)
@@ -41,27 +41,28 @@ print "Validation size: %d" % len(validation_data)
 n_in = (2*border+1)**2
 
 start = timer()
-mini_batch_size = 500
+mini_batch_size = 200
 
-ae = AutoencoderLayer(n_in=n_in, n_hidden=n_in-5)
-ae.train(training_data=pretrain_data, batch_size=mini_batch_size,
-         eta=0.25, epochs=10)
+
+net = Network([
+        AutoencoderLayer(n_in=n_in, n_hidden=n_in-10),
+        FullyConnectedLayer(n_in=n_in-10, n_out=80),
+        FullyConnectedLayer(n_in=80, n_out=1),
+    ], mini_batch_size)
+
+net.layers[0].train(training_data=pretrain_data,
+                    batch_size=mini_batch_size,
+                    eta=0.25, epochs=1)
 
 image = PIL.Image.fromarray(tile_raster_images(
-        X=ae.w.get_value(borrow=True).T,
+        X=net.layers[0].w.get_value(borrow=True).T,
         img_shape=(5, 5), tile_shape=(10, 10),
         tile_spacing=(2, 2)))
 image.show()
 
-net = Network([
-        ae,
-        FullyConnectedLayer(n_in=n_in-5, n_out=80),
-        FullyConnectedLayer(n_in=80, n_out=1),
-    ], mini_batch_size)
-
 training_data.reset()
 print '...start training'
-net.SGD(training_data=training_data, epochs=10,
+net.SGD(training_data=training_data, epochs=1,
         mini_batch_size=mini_batch_size, eta=0.01,
         validation_data=validation_data, lmbda=0.0,
         momentum=None, patience=20000, patience_increase=2,
