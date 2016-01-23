dc7a0c040f89c37bb05c3566bbc72a0adccc1632
Benutzer <s0545881@hadoop02.f4.htw-berlin.de>
2016-01-23 18:42:47 +0100

    Good Autoencoder

--- src/train_ae.py
+++ src/train_ae.py
@@ -6,7 +6,7 @@ import PIL.Image
 import cPickle
 import pdb
 import theano
-from utils import tile_raster_images
+#from utils import tile_raster_images
 from timeit import default_timer as timer
 
 from network import Network, FullyConnectedLayer, \
@@ -18,9 +18,9 @@ border = 2
 BA1 = BatchImgProcessor.load(
     X_dirpath='../../data/train_cleaned/*',
     y_dirpath='../../data/train_cleaned/',
-    batchsize=5000,
+    batchsize=500000,
     border=border,
-    limit=10,
+    limit=30,
     train_stepover=8,
     dtype=theano.config.floatX)
 pretrain_data = BA1(modus='train', random=True)
@@ -28,9 +28,9 @@ pretrain_data = BA1(modus='train', random=True)
 BatchProcessor = BatchImgProcessor.load(
     X_dirpath='../../data/train/*',
     y_dirpath='../../data/train_cleaned/',
-    batchsize=5000,
+    batchsize=500000,
     border=border,
-    limit=10,
+    limit=30,
     train_stepover=8,
     dtype=theano.config.floatX)
 training_data = BatchProcessor(modus='train', random=True)
@@ -43,30 +43,29 @@ n_in = (2*border+1)**2
 start = timer()
 mini_batch_size = 200
 
-
 net = Network([
-        AutoencoderLayer(n_in=n_in, n_hidden=n_in-10),
-        FullyConnectedLayer(n_in=n_in-10, n_out=80),
+        AutoencoderLayer(n_in=n_in, n_hidden=n_in-5),
+        FullyConnectedLayer(n_in=n_in-5, n_out=80),
         FullyConnectedLayer(n_in=80, n_out=1),
     ], mini_batch_size)
 
 net.layers[0].train(training_data=pretrain_data,
                     batch_size=mini_batch_size,
-                    eta=0.25, epochs=1)
+                    eta=0.025, epochs=10)
 
-image = PIL.Image.fromarray(tile_raster_images(
-        X=net.layers[0].w.get_value(borrow=True).T,
-        img_shape=(5, 5), tile_shape=(10, 10),
-        tile_spacing=(2, 2)))
-image.show()
+#image = PIL.Image.fromarray(tile_raster_images(
+#        X=net.layers[0].w.get_value(borrow=True).T,
+#        img_shape=(5, 5), tile_shape=(10, 10),
+#        tile_spacing=(2, 2)))
+#image.show()
 
 training_data.reset()
 print '...start training'
-net.SGD(training_data=training_data, epochs=1,
-        mini_batch_size=mini_batch_size, eta=0.01,
+net.SGD(training_data=training_data, epochs=10,
+        mini_batch_size=mini_batch_size, eta=0.025,
         validation_data=validation_data, lmbda=0.0,
         momentum=None, patience=20000, patience_increase=2,
         improvement_threshold=0.995, validation_frequency=2,
-        save_dir='./model/meta_')
+        save_dir='./model/ae_')
 end = timer()
 print "Zeit : %d" % (end-start)
