f3629ecf262b5e721c8b5d2b8735ad233f86c814
Lukas Hodel <l.hodel@de.edenspiekermann.com>
2016-02-16 17:39:21 +0100

    Refactored metric collection names fully

--- tests/config.py
+++ tests/config.py
@@ -1,4 +1,4 @@
 slow = False
-mongodb = False # new tests!
+mongodb = True # new tests!
 # the data should not be in a git-project.
 data_dir_path = '../data/' # relative to tests.py
--- tests/test_metric.py
+++ tests/test_metric.py
@@ -11,12 +11,12 @@ from collections import OrderedDict
 import config
 from src.metric import MetricRecorder
 
-@unittest.skipUnless(config.mongodb, 'MongoDB tests deactivated')
+@unittest.skipUnless(config.mongodb, 'MongoDB deactivated')
 class TestMetricRecorder(unittest.TestCase):
 
   def setUp(self):
     self.metric = MetricRecorder(
-        config_dir_path='./tests/data/spearmin_config/',
+        config_dir_path='./tests/data/spearmint_config/config.json',
         job_id=1)
 
   def tearDown(self):
@@ -31,6 +31,8 @@ class TestMetricRecorder(unittest.TestCase):
   def test_connection(self):
     self.assertIsInstance(self.metric.connection, pymongo.MongoClient)
     self.assertIsInstance(self.metric.db, pymongo.database.Database)
+    self.assertIsInstance(self.metric.trainings,
+                          pymongo.collection.Collection)
     self.assertIsInstance(self.metric.metrics,
                           pymongo.collection.Collection)
     self.assertIsInstance(self.metric.meta,
@@ -42,7 +44,20 @@ class TestMetricRecorder(unittest.TestCase):
     doc = {u'cost' : 0.22,
            u'validation_accuracy' : 0.9,
            u'epoch' : 1, u'iteration' : 4000, u'second' : 209 }
-    self.metric.record(**doc) # fany unpacking trick
+    self.metric.record(**doc) # fancy unpacking trick
+    record = self.metric.metrics.find_one({'job_id' : 1})
+    doc2 = record
+    doc2.pop('_id', None)
+    doc[u'job_id'] = 1
+    doc[u'type'] = 'train'
+    self.assertDictEqual(doc2, doc)
+
+  def test_owntype_record(self):
+    doc = {u'cost' : 0.22,
+           u'validation_accuracy' : 0.9,
+           u'epoch' : 1, u'iteration' : 4000, u'second' : 209,
+           'type' : 'pretrain_1'}
+    self.metric.record(**doc) # fancy unpacking trick
     record = self.metric.metrics.find_one({'job_id' : 1})
     doc2 = record
     doc2.pop('_id', None)
@@ -56,8 +71,8 @@ class TestMetricRecorder(unittest.TestCase):
     doc2 = {u'cost' : 0.26,
            u'validation_accuracy' : 0.89,
            u'epoch' : 2, u'iteration' : 8000, u'second' : 418 }
-    self.metric.record(**doc1) # fany unpacking trick
-    self.metric.record(**doc2) # fany unpacking trick
+    self.metric.record(**doc1) # fancy unpacking trick
+    self.metric.record(**doc2) # fancy unpacking trick
 
     doc1 = {u'job_id' : 2, u'cost' : 0.22,
            u'validation_accuracy' : 0.9,
@@ -65,8 +80,8 @@ class TestMetricRecorder(unittest.TestCase):
     doc2 = {u'job_id' : 2, u'cost' : 0.26,
            u'validation_accuracy' : 0.89,
            u'epoch' : 2, u'iteration' : 8000, u'second' : 418 }
-    self.metric.record(**doc1) # fany unpacking trick
-    self.metric.record(**doc2) # fany unpacking trick
+    self.metric.record(**doc1) # fancy unpacking trick
+    self.metric.record(**doc2) # fancy unpacking trick
 
     ln = len([x for x in self.metric.metrics.find()])
     self.assertEqual(ln, 4)
